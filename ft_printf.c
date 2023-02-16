54eb95cbe481c0eb054e1b8077c39e6175121534
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-16 12:51:06 +0100

    kbenjell

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,19 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 00:48:45 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/16 08:41:44 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/16 10:09:05 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
+	char	*i;
+
+	i = format; // i iterates on every character in the format.
+	while (*i)
+	{
+		if (*iter)
+	}
 	return (0);
 }
