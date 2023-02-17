a917a6c82cacea846c847d2876d9326243bee96b
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-17 09:30:04 +0100

    kbenjell

--- isspec.c
+++ isspec.c
@@ -1,30 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   isspec.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/27 00:48:45 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/17 09:22:27 by kbenjell         ###   ########.fr       */
+/*   Created: 2023/02/17 09:15:24 by kbenjell          #+#    #+#             */
+/*   Updated: 2023/02/17 09:19:22 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "ft_printf.h"
-
-int	ft_printf(const char *format, ...)
-{
-	char	*i;
-	char	*next;
-
-	i = format;
-	while (*i)
-	{
-		next = *(i + 1);
-		if (*i)
-			;
-		i++;
-	}
-	return (0);
-}
-
-// i iterates on every character in the format.
