87e0dd966736561d2b5c97d4f8ebfc00ab9bd041
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-18 17:44:56 +0100

    kbenjell

--- conversion_functions/ft_print_decimal.c
+++ conversion_functions/ft_print_decimal.c
@@ -6,8 +6,18 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:06 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/18 13:42:27 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/18 17:44:20 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "../printf.h"
-//spec = conversion specifier
+
+int	ft_print_decimal(void)
+{
+	decimal = ft_itoa(va_arg(ap, int));
+	ft_putstr_fd(decimal, 1);
+	return (ft_strlen(decimal));
+}
+
+//  spec = conversion specifier.
+//  decimal is a string holding an integer (base 10).
--- conversion_functions/ft_print_hexadecimal_lowercase.c
+++ conversion_functions/ft_print_hexadecimal_lowercase.c
@@ -6,8 +6,9 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:12 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/18 13:42:27 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/18 17:43:02 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../printf.h"
+
 //spec = conversion specifier
