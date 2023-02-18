726a2d4a21a9a8da5021377ca4f236e21f77fe13
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-18 13:59:47 +0100

    kbenjell

--- conversion_functions/ft_print_character.c
+++ conversion_functions/ft_print_character.c
@@ -6,7 +6,12 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:40:59 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/18 13:42:27 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/18 13:57:08 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../printf.h"
+
+int	ft_print_character(char spec)
+{
+}
+//spec = conversion specifier
