1fc920ee05c5da802862470e76f40ee363ee78b5
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-18 18:00:03 +0100

    kbenjell

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 00:48:45 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/18 14:03:20 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/18 17:59:59 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -15,8 +15,7 @@ int	ft_isspec(char c)
 {
 	if (ft_strchr("cspdiuxX%", c))
 		return (1);
-	else
-		return (0);
+	return (0);
 }
 
 int	ft_invalid_spec(char c)
@@ -24,9 +23,9 @@ int	ft_invalid_spec(char c)
 	char	*message;
 
 	message = "Error: Invalid conversion specifier : '";
-	ft_putstr(message);
-	ft_putchar(c);
-	ft_putstr("'");
+	ft_putstr_fd(message, 1);
+	ft_putchar_fd(c, 1);
+	ft_putstr_fd("'", 1);
 }
 
 int	ft_convert(char spec)
@@ -37,6 +36,7 @@ int	ft_convert(char spec)
 
 int	ft_printf(const char *format, ...)
 {
+	int		output_length;
 	char	*i;
 	char	*next;
 	va_list	ap;
@@ -49,16 +49,16 @@ int	ft_printf(const char *format, ...)
 		if (*i == '%')
 		{
 			if (isspec(*next))
-				ft_convert(next);
+				ft_convert(*next);
 			else
-				ft_invalid_spec(next);
+				ft_invalid_spec(*next);
 		}
 		else
 			ft_putchar(*i);
 		i++;
 	}
 	va_end(va_list ap);
-	return (0);
+	return (output_length);
 }
 
 // i iterates on every character in the format.
