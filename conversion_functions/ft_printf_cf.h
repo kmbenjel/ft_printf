b979d075ca19fcdaa8ff3fbd75ff3786fe5cf193
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-19 15:34:05 +0100

    kbenjell

--- conversion_functions/ft_printf_cf.h
+++ conversion_functions/ft_printf_cf.h
@@ -1,8 +1,20 @@
-ft_print_character.c
-ft_print_decimal.c
-ft_print_hexadecimal_lowercase.c
-ft_print_hexadecimal_uppercase.c
-ft_print_percent.c
-ft_print_pointer.c
-ft_print_string.c
-ft_print_unsigned.c
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_cf.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 15:26:34 by kbenjell          #+#    #+#             */
+/*   Updated: 2023/02/19 15:33:12 by kbenjell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_CF_H
+# define FT_PRINTF_CF_H
+
+# include "../printf.h"
+
+int ft_print_character int ft_print_decimal int ft_print_hexadecimal_lowercase int ft_print_hexadecimal_uppercase int ft_print_percent int ft_print_pointer int ft_print_string int ft_print_unsigned
+
+#endif
