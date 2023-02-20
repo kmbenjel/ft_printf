ff860e249d5cb851240982b72f8d87858736c96d
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-20 05:50:09 +0100

    kbenjell

--- conversion_functions/ft_print_hexadecimal_lowercase.c
+++ conversion_functions/ft_print_hexadecimal_lowercase.c
@@ -6,13 +6,12 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:12 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/19 21:31:20 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/19 21:55:10 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf_cf.h"
 
-int	ft_print_hexadecimal_lowercase(void)
+int	ft_print_hexadecimal_lowercase(va_list ap)
 {
-	hexadecimal =
+	hexadecimal = ft_strjoin("x0");
 }
-//spec = conversion specifier
