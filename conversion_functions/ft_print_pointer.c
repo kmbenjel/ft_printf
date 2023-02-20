4d2269b0169e5d1b9457e8c92d5f00ca91672960
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-20 17:56:16 +0100

    kbenjell

--- conversion_functions/ft_print_pointer.c
+++ conversion_functions/ft_print_pointer.c
@@ -6,12 +6,17 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:41 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/20 17:02:56 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/20 17:52:51 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf_cf.h"
 
 int	ft_print_pointer(va_list ap)
 {
-	int	count;
+	int		count;
+	char	*hex;
+
+	hex = ft_strjoin("0x", va_arg(ap, void *));
+	count = ft_strlen(hex);
+	return (count);
 }
