b74ed8a3509af4a8d97c15dc1da5e7372ed93d6f
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-20 18:16:25 +0100

    kbenjell

--- conversion_functions/ft_print_string.c
+++ conversion_functions/ft_print_string.c
@@ -6,10 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:44 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/19 16:30:57 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/20 18:16:23 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "../printf.h"
 #include "ft_printf_cf.h"
-
-//spec = conversion specifier
