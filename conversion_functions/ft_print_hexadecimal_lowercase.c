cc53a6e1f621a6e90dc6c1fb93aecd200ddafb9c
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-20 11:49:46 +0100

    kbenjell

--- conversion_functions/ft_print_hexadecimal_lowercase.c
+++ conversion_functions/ft_print_hexadecimal_lowercase.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:12 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/20 11:44:20 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/20 11:46:53 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf_cf.h"
@@ -14,11 +14,18 @@
 static char	*ft_reverse_string(const char *str)
 {
 	char	*result;
+	int		i;
+
+	i = 0;
+	result = malloc(ft_strlen(str) + 1);
+	if (!result)
+		return (NULL);
 }
 
-// Digits for the hexadecimal representation will be collected from the rigth to the left using the remainders of the division by 16,
+// Digits for the hexadecimal representation will be collected from the rigth to
+// the left using the remainders of the division by 16,
 //	so we will need to reverse that order,
-//	so I created FT_REVERSE_STRING for that purpose.
+//	for that purpose FT_REVERSE_STRING is there.
 
 int	ft_print_hexadecimal_lowercase(va_list ap)
 {
