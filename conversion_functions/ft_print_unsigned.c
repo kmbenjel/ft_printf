7031a86dd1a89ffdde0922d803f37ca15c0bedb1
Khalid Benjelloun <kbenjell@student.1337.ma>
2023-02-21 15:44:13 +0100

    kbenjell

--- conversion_functions/ft_print_unsigned.c
+++ conversion_functions/ft_print_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:46 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/20 21:32:04 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/21 15:41:34 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,20 @@ static char	*uitoa(unsigned int n)
 
 {
 	char *str;
-	char *p;
+	int remainder = 0;
+	int i = 0;
 
 	str = malloc(9);
-	p = str;
 	if (!str)
 		return (NULL);
 	while (n)
 	{
+		remainder = n % 10;
+		str[i++] = remainder + '0';
+		n /= 10;
 	}
+	str[i] = '\0';
+	return (ft_reverse_string(str));
 }
+
+//  UITOA stands for: Unsigned Integer to ASCII.
