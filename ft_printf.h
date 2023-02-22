27b66bf4d511b0c031967f9c6a684864723cd237
Khalid Benjelloun <khalidmailing@gmail.com>
2023-02-23 00:12:06 +0100

    kbenjell

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 09:25:07 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/22 14:22:04 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/23 00:09:44 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 # include "conversion_functions/ft_printf_cf.h"
 # include <stdarg.h>
 # include <unistd.h>
-int	ft_printf(const char *, ...);
+
+int	ft_printf(const char *format, ...);
+
 #endif
