302448ae20ceaea04516804fca0308e33baad266
Khalid Benjelloun <khalidmailing@gmail.com>
2023-02-22 15:40:04 +0100

    kbenjell

--- conversion_functions/ft_print_percent.c
+++ conversion_functions/ft_print_percent.c
@@ -6,16 +6,13 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:37 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/20 20:13:27 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/22 15:39:29 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf_cf.h"
 
-int	ft_print_percent(va_list ap)
+int	ft_print_percent(void)
 {
-	char	percent;
-
-	percent = va_arg(ap, int);
-	ft_putchar_fd(percent, 1);
+	ft_putchar_fd('%', 1);
 	return (1);
 }
--- conversion_functions/ft_printf_cf.h
+++ conversion_functions/ft_printf_cf.h
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 15:26:34 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/21 16:25:30 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/22 15:39:08 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_print_character(va_list ap);
 int		ft_print_decimal(va_list ap);
 int		ft_print_hexadecimal_lowercase(va_list ap);
 int		ft_print_hexadecimal_uppercase(va_list ap);
-int		ft_print_percent(va_list ap);
+int		ft_print_percent(void);
 int		ft_print_pointer(va_list ap);
 int		ft_print_string(va_list ap);
 int		ft_print_unsigned(va_list ap);
