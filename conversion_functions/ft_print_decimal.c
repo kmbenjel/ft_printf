8057975053f1f52a063c4fb18f3e96905e4f3d4f
Khalid Benjelloun <khalidmailing@gmail.com>
2023-02-22 17:19:27 +0100

    kbenjell

--- conversion_functions/ft_print_decimal.c
+++ conversion_functions/ft_print_decimal.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:06 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/22 17:14:25 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/22 17:17:09 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	ft_print_decimal(va_list ap)
 	decimal = ft_itoa(va_arg(ap, int));
 	count = ft_strlen(decimal);
 	ft_putstr_fd(decimal, 1);
+	free(decimal);
 	return (count);
 }
 
