444c4ce548fe6e68b58d30bc71422ed468710795
Khalid Benjelloun <khalidmailing@gmail.com>
2023-02-23 00:02:01 +0100

    kbenjell

--- conversion_functions/ft_print_unsigned.c
+++ conversion_functions/ft_print_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:46 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/22 23:53:56 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/23 00:00:33 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static char	*ft_uitoa(unsigned int uiarg)
 
 	remainder = 0;
 	i = 0;
-	str = malloc(9);
+	str = malloc(11);
 	free_helper = str;
 	if (!uiarg)
 		str[i++] = '0';
