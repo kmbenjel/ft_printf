4002072ec2defe1e8f7b2e1a6756ce7d22664a24
Khalid Benjelloun <khalidmailing@gmail.com>
2023-02-22 23:11:32 +0100

    kbenjell

--- conversion_functions/ft_print_string.c
+++ conversion_functions/ft_print_string.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:44 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/22 23:05:09 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/22 23:10:13 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf_cf.h"
@@ -16,15 +16,11 @@ int	ft_print_string(va_list ap)
 	char	*str;
 	int		count;
 
-	count = 0;
-	if (ap)
-		str = va_arg(ap, char *);
+	str = va_arg(ap char *);
 	if (str)
-	{
 		ft_putstr_fd(str, 1);
-		count = ft_strlen(str);
-	}
 	else
-		ft_putstr_fd("0");
+		ft_putstr_fd("nullllll", 1);
+	count = ft_strlen(str);
 	return (count);
 }
