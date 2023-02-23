a7602b0baea0174909bb251c86cda54381d6b1a5
Khalid Benjelloun <khalidmailing@gmail.com>
2023-02-23 01:51:10 +0100

    kbenjell

--- conversion_functions/ft_print_unsigned.c
+++ conversion_functions/ft_print_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:41:46 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/23 00:00:33 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/23 01:49:16 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,8 @@ static char	*ft_uitoa(unsigned int uiarg)
 	free(free_helper);
 	return (str);
 }
+
+// Max unsigned int length is 10 digits.
 
 int	ft_print_unsigned(va_list ap)
 {
