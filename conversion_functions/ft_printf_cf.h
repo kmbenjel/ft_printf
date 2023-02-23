b36f30247d6b5e79287812997be73b582c2e45ed
Khalid Benjelloun <khalidmailing@gmail.com>
2023-02-23 01:47:51 +0100

    kbenjell

--- conversion_functions/ft_printf_cf.h
+++ conversion_functions/ft_printf_cf.h
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 15:26:34 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/02/22 15:39:08 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/02/23 01:45:27 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 # include "../libft/libft.h"
 # include <stdarg.h>
 
-int		ft_print_character(va_list ap);
+int		ft_print_percent(void);
+int		ft_print_string(va_list ap);
 int		ft_print_decimal(va_list ap);
+int		ft_print_pointer(va_list ap);
+int		ft_print_unsigned(va_list ap);
+char	*ft_uitoh(unsigned int uiarg);
+int		ft_print_character(va_list ap);
+char	*ft_reverse_string(const char *str);
 int		ft_print_hexadecimal_lowercase(va_list ap);
 int		ft_print_hexadecimal_uppercase(va_list ap);
-int		ft_print_percent(void);
-int		ft_print_pointer(va_list ap);
-int		ft_print_string(va_list ap);
-int		ft_print_unsigned(va_list ap);
-char	*ft_reverse_string(const char *str);
-char	*ft_uitoh(unsigned int uiarg);
+
 #endif
